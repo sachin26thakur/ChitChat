4b2356af468c76be14ca2b1dd8e9a91f893605dd
Amit <amit.gupta5@globallogic.com>
2016-02-14 23:28:04 +0530

    Add Validation in SignupViewController

--- CodersChat/CodersChat/SignUpViewController.m
+++ CodersChat/CodersChat/SignUpViewController.m
@@ -10,12 +10,15 @@
 #import "Constants.h"
 #import "LoginViewController.h"
 #import "SelectLanguageViewController.h"
+#import "Utility.h"
 
 @interface SignUpViewController ()
 @property (strong, nonatomic) IBOutlet UITextField *userFullName;
 @property (strong, nonatomic) IBOutlet UITextField *userName;
 @property (strong, nonatomic) IBOutlet UITextField *phoneNumber;
 @property (strong, nonatomic) IBOutlet UITextField *password;
+@property (strong, nonatomic) IBOutlet UITextField *confirmPassword;
+
 @property (strong, nonatomic) IBOutlet UIPickerView *languagePickerVIew;
 @property (strong, nonatomic) IBOutlet UIButton *registerButton;
 @property (strong, nonatomic) IBOutlet UIButton *previousBtn;
@@ -35,10 +38,50 @@
 }
 - (IBAction)nextButtonClicked:(id)sender
 {
-    [self performSegueWithIdentifier:@"languageScreenSegue" sender:self];
-    
-
+    if ([self validateAllFields]) {
+        [self performSegueWithIdentifier:@"languageScreenSegue" sender:self];
+    }
 }
+
+-(BOOL)validateAllFields {
+    BOOL isValidate = YES;
+    if (![Utility isValidName:self.userFullName.text]) {
+        [self showAlertWithTitle:@"Name" andDescription:@"Please enter valid name."];
+        isValidate = NO;
+    }
+    else if (![Utility isValidName:self.phoneNumber.text]) {
+        [self showAlertWithTitle:@"Phone Number" andDescription:@"Please enter valid phone number."];
+        isValidate = NO;
+
+    }
+    else if (![Utility isValidName:self.userName.text]) {
+        [self showAlertWithTitle:@"User Number" andDescription:@"Please enter valid user name."];
+        isValidate = NO;
+
+    }
+    else if (![Utility isValidName:self.password.text]) {
+        [self showAlertWithTitle:@"User Number" andDescription:@"Please enter your password."];
+        isValidate = NO;
+
+    }
+    else if (![Utility isValidName:self.confirmPassword.text]) {
+        [self showAlertWithTitle:@"User Number" andDescription:@"Please enter confirm password."];
+        isValidate = NO;
+
+    }
+    else if (![self.password.text isEqualToString:self.confirmPassword.text]){
+        [self showAlertWithTitle:@"Confirm Password" andDescription:@"Password and confirm password should be same."];
+        isValidate = NO;
+
+    }
+    return isValidate;
+}
+-(void)showAlertWithTitle:(NSString *)title andDescription:(NSString *)description {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title message:description delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+    [alertView show];
+}
+
+
 - (IBAction)previousButtonClicked:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
